ea1321176250cda65b3d12647a7c1dbf1a6e0b06
CaioVSRodrigues <caiovsrodriguess@gmail.com>
2023-09-28 13:54:54 -0300

    OrderItem, many_to_many association with extra attributes PART 2

--- src/main/java/com/caiovinicius/course/config/TestConfig.java
+++ src/main/java/com/caiovinicius/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.caiovinicius.course.entities.Category;
 import com.caiovinicius.course.entities.Order;
+import com.caiovinicius.course.entities.OrderItem;
 import com.caiovinicius.course.entities.Product;
 import com.caiovinicius.course.entities.User;
 import com.caiovinicius.course.entities.enums.OrderStatus;
 import com.caiovinicius.course.repositories.CategoryRepository;
+import com.caiovinicius.course.repositories.OrderItemRepository;
 import com.caiovinicius.course.repositories.OrderRepository;
 import com.caiovinicius.course.repositories.ProductRepository;
 import com.caiovinicius.course.repositories.UserRepository;
@@ -34,7 +36,10 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private ProductRepository productRepository;
-
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		
@@ -71,6 +76,13 @@ public class TestConfig implements CommandLineRunner{
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 	}
 
 }
---
+++ src/main/java/com/caiovinicius/course/repositories/OrderItemRepository.java
@@ -0,0 +1,9 @@
+package com.caiovinicius.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.caiovinicius.course.entities.OrderItem;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long>{
+
+}
